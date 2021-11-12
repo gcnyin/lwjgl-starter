d3bb6792c4ebf492a996942f72da7ec68e338867
gcnyin <guchounongyin@gmail.com>
2021-11-12 22:11:27 +0800

    update

--- app/src/main/java/com/github/gcnyin/HelloWorld.java
+++ app/src/main/java/com/github/gcnyin/HelloWorld.java
@@ -1,17 +1,18 @@
 package com.github.gcnyin;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWGamepadState;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.system.MemoryUtil;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
 import static org.lwjgl.opengl.GL.createCapabilities;
 import static org.lwjgl.opengl.GL11.*;
 
 public class HelloWorld {
 
     GLFWKeyCallback keyCallback;
+    GLFWGamepadState state;
 
     public void run() {
         if (!glfwInit()) {
@@ -21,8 +22,7 @@ public class HelloWorld {
 
         long window = glfwCreateWindow(800, 600, Version.getVersion(), MemoryUtil.NULL, MemoryUtil.NULL);
 
-        if (window == 0L) {
-            System.out.println("window == 0L");
+        if (window == MemoryUtil.NULL) {
             glfwTerminate();
             System.exit(-1);
         }
@@ -32,10 +32,13 @@ public class HelloWorld {
 
         glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
             public void invoke(long window, int key, int scancode, int action, int mods) {
-                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
                     glfwSetWindowShouldClose(window, true);
+                }
             }
         });
+
+        glClearColor(1.0f, 0.5f, 0.0f, 0.0f);
 
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT);
